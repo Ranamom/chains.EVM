96128181e77fe464bfc9518b9ed618c36a779b26
vinaydawani <vdawani6@gmail.com>
2021-07-11 05:54:08 -0400

    .

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div> -->
   <navbar></navbar>
-  <!-- <router-view /> -->
 </template>
 
 <script>
