177871ab81559f755929a33f0d6e6251674a020a
vinaydawani <vdawani6@gmail.com>
2021-07-14 12:43:03 -0400

    moved pages to scrollContainer for abstraction

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,28 @@
 <template>
-  <navbar></navbar>
-  <vue-scroll-snap :fullscreen="true" :horizontal="true">
-    <div class="item"><Home></Home></div>
-    <div class="item"><About></About></div>
-  </vue-scroll-snap>
+  <div>
+    <navbar></navbar>
+    <scrollContainer></scrollContainer>
+  </div>
 </template>
 
 <script>
-import Home from "@/views/Home.vue";
-import About from "@/views/About.vue";
+import scrollContainer from "@/views/scrollContainer.vue";
 import navbar from "@/components/navbar.vue";
-import VueScrollSnap from "vue-scroll-snap";
+
 export default {
   components: {
     navbar,
-    Home,
-    About,
-    VueScrollSnap,
+    scrollContainer,
   },
 };
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  color: #1a1a1a;
+} */
 </style>
---
+++ src/views/scrollContainer.vue
@@ -0,0 +1,21 @@
+<template>
+  <vue-scroll-snap :fullscreen="true" :horizontal="true">
+    <div class="item"><Home></Home></div>
+    <div class="item"><About></About></div>
+  </vue-scroll-snap>
+</template>
+
+<script>
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
+import VueScrollSnap from "vue-scroll-snap";
+
+export default {
+  name: "scrollContainer",
+  components: {
+    VueScrollSnap,
+    Home,
+    About,
+  },
+};
+</script>
