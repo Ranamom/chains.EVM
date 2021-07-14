5d022369d5402bf5a5fd14ad31bce77ed42931db
vinaydawani <vdawani6@gmail.com>
2021-07-14 01:23:00 -0400

    horizontal scrolling and snap

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <navbar></navbar>
+  <vue-scroll-snap :fullscreen="true" :horizontal="true">
+    <div class="item"><Home></Home></div>
+    <div class="item"><About></About></div>
+  </vue-scroll-snap>
 </template>
 
 <script>
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
 import navbar from "@/components/navbar.vue";
+import VueScrollSnap from "vue-scroll-snap";
 export default {
   components: {
     navbar,
+    Home,
+    About,
+    VueScrollSnap,
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,4 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-  </div>
   <div>
     <button @click="connect">
       <span>Address: {{ getUser }}</span>
