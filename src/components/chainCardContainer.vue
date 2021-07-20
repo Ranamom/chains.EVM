a870387ab68d2d70367a1c51ae7d5752eae387de
vinaydawani <vdawani6@gmail.com>
2021-07-20 02:43:32 -0400

    chains responsive

--- src/components/chainCardContainer.vue
+++ src/components/chainCardContainer.vue
@@ -1,11 +1,15 @@
 <template>
   <div
     class="
-      grid grid-cols-4
+      grid grid-cols-1
+      md:grid-cols-2
+      lg:grid-cols-3
+      xl:grid-cols-4
       gap-x-4 gap-y-8
       grid-flow-row
       justify-items-center
-      px-28
+      px-12
+      xl:px-28
       mt-36
       mb-18
       z-40
