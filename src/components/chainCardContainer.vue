87d857c90e02d7eb3c7a0a848cd871527c78a63d
vinaydawani <vdawani6@gmail.com>
2021-07-21 18:01:09 -0400

    better reactivity?

--- src/components/chainCardContainer.vue
+++ src/components/chainCardContainer.vue
@@ -4,7 +4,7 @@
       grid grid-cols-1
       md:grid-cols-2
       lg:grid-cols-3
-      xl:grid-cols-4
+      1.5xl:grid-cols-4
       gap-x-4 gap-y-8
       grid-flow-row
       justify-items-center
--- tailwind.config.js
+++ tailwind.config.js
@@ -69,6 +69,9 @@ module.exports = {
       width: {
         76: "19rem",
       },
+      screens: {
+        "1.5xl": "1440px",
+      },
     },
   },
   variants: {
